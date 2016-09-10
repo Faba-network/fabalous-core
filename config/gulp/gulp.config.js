ebff43c1fd343ddef000a6f58551ab813dddaa5b
Jörg Wasmeier <j.wasmeier@faba.de>
2016-09-10 10:53:16 +0200

    Fix Typescript build

---
+++ config/gulp/gulp.config.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+var path = require('path');
+
+var absolutePath = path.join(__dirname, '../../');
+
+gulp.task('clean', function() {
+    return gulp.src(absolutePath + 'lib/')
+        .pipe(clean());
+});
+
+gulp.task('copy_src_to_lib', function() {
+    return gulp.src(absolutePath + 'lib/src/**')
+        .pipe(gulp.dest(absolutePath + 'lib/'));
+});
+
+gulp.task('remove_src_folder', function() {
+    return gulp.src(absolutePath + 'lib/src', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('remove_node_modules_folder', function() {
+    return gulp.src(absolutePath + 'lib/node_modules', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('copy_config_folder', function() {
+    return gulp.src(absolutePath + 'config/**')
+        .pipe(gulp.dest(absolutePath + 'config/'));
+});
+
+gulp.task('copy_package_json', function() {
+    return gulp.src('package.json')
+        .pipe(gulp.dest(absolutePath + 'lib/'));
+});
+
+jasmine = require('gulp-jasmine');
+
+gulp.task('test', function(){
+    gulp.src('test/FabaServerSpec.js')
+        .pipe(jasmine())
+});
