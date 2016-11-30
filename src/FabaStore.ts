208e42c3a1fc89aa1203fc503f00f88e216e0912
Jörg Wasmeier <j.wasmeier@faba.de>
2016-11-30 17:42:33 +0100

    Add FabaStore

--- src/FabaStore.ts
+++ src/FabaStore.ts
@@ -1,10 +1,34 @@
-/**
- * Created by creativecode on 29.11.16.
- */
+import FabaStoreUpdateEvent from "./FabaStoreUpdateEvent";
 
-export default class FabaStore {
-    constructor() {
+declare var require;
+const Baobab = require("Baobab");
 
+export default class FabaStore<TProp> {
+    private bTree: any;
+    private cursor: any;
+    protected bData: any;
+
+    get tree() {
+        return this.bTree;
     }
 
+    get data(): TProp {
+        return this.bData;
+    }
+
+    constructor(jsonObject: any) {
+        this.bTree = new Baobab(jsonObject);
+        this.cursor = this.tree.select();
+        this.bData = this.cursor.get();
+
+        this.cursor.on("update", (e) => {
+            new FabaStoreUpdateEvent(e).dispatch();
+        });
+    }
+
+    set(path: string, value: any, update: boolean = true) {
+        let arrPath = path.split(".");
+        this.cursor.set(arrPath, value);
+    }
 }
+
---
+++ src/FabaStoreUpdateEvent.ts
@@ -0,0 +1,11 @@
+import FabaEvent from "./FabaEvent";
+
+export default class FabaStoreUpdateEvent extends FabaEvent {
+    data: any;
+
+    constructor(e: any) {
+        super("FabaStoreUpdateEvent");
+
+        this.data = e;
+    }
+}
