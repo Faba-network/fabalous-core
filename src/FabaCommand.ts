d906acbe37a0ed13a3704166d0a79923805d9557
Jörg Wasmeier <j.wasmeier@faba.de>
2016-12-19 10:23:29 +0100

    Simplyfy store access

--- src/FabaCommand.ts
+++ src/FabaCommand.ts
@@ -1,14 +1,20 @@
 import FabaEvent from "./FabaEvent";
 import FabaCoreRuntime from "./FabaCoreRuntime";
-
+import FabaStore from "./FabaStore";
 export default class FabaCommand<TStore> {
-    store: TStore;
+    private store: FabaStore<TStore>;
+    data: TStore;
 
-    constructor(store: TStore) {
+    constructor(store) {
         this.store = store;
+        this.data = store.data;
     }
 
     protected sendToEndpoint(event:FabaEvent, endPoint?:string):void {
         FabaCoreRuntime.sendToEndpoint(event, endPoint);
+    }
+
+    protected setStore(path: string, value: any, update: boolean = true) {
+        this.store.set(path,value,update);
     }
 }
