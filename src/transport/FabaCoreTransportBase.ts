313512c83bf6467f09890917f0921fae765d6efb
Jörg Wasmeier <j.wasmeier@faba.de>
2017-04-02 15:02:42 +0200

    Transportbase

--- src/transport/FabaCoreTransportBase.ts
+++ src/transport/FabaCoreTransportBase.ts
@@ -1,29 +1,47 @@
 import FabaEvent from "./../FabaEvent";
 
+/**
+ * This class is used by all Fabalous Endpoints ansd should be extended by a new Transport method like Rest or Websocket.
+ */
 export default class FabaCoreTransportBase {
+    /** TODO: Check if needed
     private evnid: string;
-
     private runningQuerysNew: Array<any> = [];
     private runningQuerys: Array<any>;
 
     constructor() {
     }
 
+    /**
+     * Serialize the FabaEvent
+     *
+     * @param event
+     * @param compress
+     * @returns {string}
+     */
     protected prepareEventToSend(event: FabaEvent, compress: boolean = true): string {
-        var qId = Math.random();
+        // TODO: Check if needed
 
+        var qId = Math.random();
         //this.runningQuerysNew.push({e: event, q: qId, v: 0, clb: event.callBack});
 
         var serilizedData: string = JSON.stringify(event);
         return serilizedData;
     }
 
+    /**
+     * Need to Override
+     * @param incomingMsg
+     */
     protected messageHandler(incomingMsg: any): void {
         //data = JSON.parse(incomingMsg);
     }
 
+    /**
+     * Need to override
+     * @param event
+     */
     public send(event: FabaEvent) {
-
     }
 
 }
