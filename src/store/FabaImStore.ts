72e141d4391ee1c6b36a1f3a7b264249460ae121
Jörg Wasmeier <j.wasmeier@faba.de>
2017-11-07 16:32:04 +0100

    Create new ImFaba Store

---
+++ src/store/FabaImStore.ts
@@ -0,0 +1,46 @@
+import FabaStoreUpdateEvent from "../event/FabaStoreUpdateEvent";
+
+export interface IFabaImStoreOptions{
+    updateInterval:number;
+    freeze:boolean;
+}
+
+export default class FabaImStore<TProp>{
+    data:TProp;
+    patchData:Array<any>;
+
+    options:IFabaImStoreOptions = {
+        freeze:false,
+        updateInterval:20
+    }
+
+    constructor(jsonObject: TProp, options?:IFabaImStoreOptions){
+        this.data = jsonObject;
+        this.patchData = []; 
+
+        if (options) this.options = options;
+        setInterval(this.updatePatchData, this.options.updateInterval);
+    }
+
+    update(obj:TProp){
+        this.patchData.push(obj);
+    }
+
+    commit(){
+        new FabaStoreUpdateEvent(this.data).dispatch();
+    }
+
+    private updatePatchData(){
+        if (this.patchData.length == 0) return;
+        
+        this.patchData.forEach(element => {
+            this.data = Object.assign(this.data, element);
+        });
+
+        this.patchData = [];
+
+        if (this.options.freeze) Object.freeze(this.data);
+
+        this.commit();
+    }
+}
